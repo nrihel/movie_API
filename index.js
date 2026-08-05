be43cb7ba289057f13b807f94881a3a703b6bab4
nrihel <nrihel@gmail.com>
2026-08-05 06:50:27 -0600

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 // API: http://www.omdbapi.com/?apikey=db4e97d3
 const movieListEl = document.querySelector(".movie-list");
+const titleInput = document.getElementById("Title");
 
 async function main() {
-    const movies = await fetch(`http://www.omdbapi.com/?apikey=db4e97d3&s=${Title.value}`);
-    const movieData = await movies.json();document.getElementById("Title");
-    movieListEl.innerHTML = movieData.map((movie) => movieHTML(movie)).join("");
+    const Title =
+        titleInput.value || localStorage.getItem("searchTitle");
+
+    if (!movieListEl) {
+        localStorage.setItem("searchTitle", Title);
+        window.location.href = "./movie.html";
+        return;
+    }
+
+    const response = await fetch(`http://www.omdbapi.com/?apikey=db4e97d3&s=${Title}`);
+    const movieData = await response.json();
+    const movies = movieData.Search || [];
+
+    movieListEl.innerHTML = movies
+        .map((movie) => movieHTML(movie))
+        .join("");
 }
 
 main();
 
 function showMovie(title) {
     localStorage.setItem("Title", title);
-    window.location.href = `${window.location.origin}/movie.html`
+    window.location.href = `${window.location.origin}/movie.html`;
 }
 
 function movieHTML(movie) {
